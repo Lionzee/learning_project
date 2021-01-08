bd91b50ffaab4ad5e6965ade96172b3ddf2ae438
achmadsyfdin <achmadsyfdin@gmail.com>
2021-01-08 22:05:42 +0700

    Update
    + Works Model and Migrate Define

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Work extends Model
 {
-    //
+    protected $table = 'wprks';
+
+    protected $fillable = ['user_id','quiz_id','is_visible','is_finished','total_time'];
+
+    public function quiz(){
+        return $this->belongsTo(Quiz::class, 'quiz_id','id');
+    }
 }
--- database/migrations/2021_01_08_133407_create_works_table.php
+++ database/migrations/2021_01_08_133407_create_works_table.php
@@ -15,7 +15,16 @@ class CreateWorksTable extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('quiz_id');
+            $table->boolean('is_visible')->default(false);
+            $table->boolean('is_finished')->default(false);
+            $table->unsignedInteger('total_time')->default(0);
             $table->timestamps();
+
+            $table->foreign('quiz_id')
+                ->references('id')->on('quizzes')
+                ->onDelete('cascade');
         });
     }
 
